b82f8d91983cc6260f211e75d092cd969eccb5f0
Adrian Perez <adrianperez.deb@gmail.com>
2009-09-26 16:54:26 -0400

    Move Cart to a partial

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,7 @@
 class StoreController < ApplicationController
   def index
     @products = Product.find_products_for_sale
+    @cart = find_cart
   end
 
   def add_to_cart
@@ -8,23 +9,27 @@ class StoreController < ApplicationController
       product = Product.find(params[:id])
     rescue ActiveRecord::RecordNotFound
       logger.error("Attempt to access invalid product #{params[:id]}")
-      flash[:notice] = "Invalid product"
-      redirect_to :action => :index
+      redirect_to_index "Invalid product"
     else
       @cart = find_cart
       @cart.add_product(product)
+      redirect_to_index
     end
   end
 
   def empty_cart
     session[:cart].items.clear
-    flash[:notice] = "Your Cart is currently empty"
-    redirect_to :action => :index
+    redirect_to_index "Your Cart is currently empty"
   end
   
   private
   def find_cart
     session[:cart] ||= Cart.new
+  end
+
+  def redirect_to_index(msg = nil)
+    flash[:notice] = msg if msg
+    redirect_to :action => index
   end
 
 end
