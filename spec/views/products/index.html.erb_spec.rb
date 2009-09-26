d933b67ab0aec211bf0a15ea3b658ec98301acf9
Adrian Perez <adrianperez.deb@gmail.com>
2009-09-26 13:16:59 -0400

    Fix RSpec test of index view for Product to use number_to_currency

--- spec/views/products/index.html.erb_spec.rb
+++ spec/views/products/index.html.erb_spec.rb
@@ -23,7 +23,7 @@ describe "/products/index.html.erb" do
   it "renders a list of products" do
     render
     response.should have_tag("tr>td", "value for title".to_s, 2)
-    response.should have_tag("tr>td", 9.99.to_s, 2)
+    response.should have_tag("tr>td", number_to_currency(9.99), 2)
     response.should have_tag("tr>td", "value for description".to_s, 2)
     response.should have_tag("tr>td", "value for image_url".to_s, 2)
   end
