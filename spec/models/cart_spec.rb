ad53bbf2ecc5f55a7d8c5dbbdd15064b4fea6ba6
Adrian Perez <adrianperez.deb@gmail.com>
2009-09-26 15:48:51 -0400

    Add smart handling of products by increasing quantity instead of duplicating them

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -7,13 +7,25 @@ describe Cart do
     @valid_attributes = {
       
     }
+    @cart = Cart.new
+    @product = Product.new(:title => "Mp3 Player",
+                           :description => "Awesome Mp3 player",
+                           :price => 30.95)
   end
 
   it "should create a new instance given valid attributes" do
     Cart.new
   end
 
-  it "should add a new product" do
-    # TODO: implement
+  it "should add a new product to Cart" do
+    @cart.add_product(@product)
+    @cart.items[0].product.should == @product
+    @cart.items.size.should == 1
+  end
+
+  it "should increment quantity if product already in Cart" do
+    2.times { @cart.add_product(@product) }
+    @cart.items.size.should == 1
+    @cart.items[0].quantity.should == 2
   end
 end
