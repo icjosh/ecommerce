08dd7b2717d498a407abc2e812e42a42ed0fc44c
Adrian Perez <adrianperez.deb@gmail.com>
2009-09-26 19:32:42 -0400

    Rewrite most views to use application layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def title(content)
+    content_for(:title, content)
+  end
+
+  def head(&content)
+    content_for(:head, content)
+  end
+
 end
