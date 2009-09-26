c512eab026cca7652e6c94cd49bdcfde89ca88e0
Adrian Perez <adrianperez.deb@gmail.com>
2009-09-26 15:40:08 -0400

    Add CartItem model

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,2 +1,20 @@
-class CartItem < ActiveRecord::Base
+class CartItem
+  attr_reader :product, :quantity
+
+  def initialize(product)
+    @product = product
+    @quantity = 1
+  end
+
+  def increment_quantity
+    @quantity += 1
+  end
+
+  def title
+    @product.title
+  end
+
+  def price
+    @product.price * @quantity
+  end
 end
--- spec/models/cart_item_spec.rb
+++ spec/models/cart_item_spec.rb
@@ -2,12 +2,18 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe CartItem do
   before(:each) do
-    @valid_attributes = {
-      
-    }
+    @product = Product.new(:title => "Mp3 Player",
+                           :description => "Awesome Mp3 player",
+                           :price => 30.95
+                           )
   end
-
-  it "should create a new instance given valid attributes" do
-    CartItem.create!(@valid_attributes)
+  
+  
+  # TODO: refactor all this tests into factories/fixtures
+  it "should wrap a product into a CartItem" do
+    @cart_item = CartItem.new(@product)
+    @cart_item.title.should == "Mp3 Player"
+    @cart_item.price.should == 30.95
+    @cart_item.increment_quantity.should == 2  
   end
 end
