03b2e5b9c0fd6e73e260359b99197b7e681d791f
Adrian Perez <adrianperez.deb@gmail.com>
2009-09-26 21:12:37 -0400

    Add unobtrusive Ajax support for "Empty Cart" action

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function() {
+    $("#cart_actions form").submit(function() {
+        $.post($(this).attr("action"), $(this).serialize(), null, "script");
+        return false;
+    })
+
+})
