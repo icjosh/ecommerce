d2bdcdaa71b13cd16f9f7ee30f17ac5ddd9a140a
Adrian Perez <adrianperez.deb@gmail.com>
2009-09-30 19:03:07 -0400

    Stub out Order#save

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,8 @@ class Order < ActiveRecord::Base
   # TODO: Add Cart and CartItem to database to make associations work
   # belongs_to :cart
   attr_accessor :card_number, :card_verification
+
+  def save
+    # TODO: Implement
+  end
 end
