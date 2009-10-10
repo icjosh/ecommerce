0ac6832a2a0a12dbb5b0121f5e3907be7ca33e06
Adrian Perez <adrianperez.deb@gmail.com>
2009-10-10 15:29:11 -0400

    Add pagination for products and populate DB.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.xml
   def index
-    @products = Product.all
+    @products = Product.paginate(:per_page => 10, :page => params[:page])
 
     respond_to do |format|
       format.html # index.html.erb
