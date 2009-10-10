494a8c9f8561a5291b09814a5706bec3a174010b
Adrian Perez <adrianperez.deb@gmail.com>
2009-10-10 16:29:13 -0400

    Add "authlogic" gem to config/environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -19,6 +19,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem "authlogic", :lib => false, :version => ">=2.1.2"
   config.gem "mislav-will_paginate", :lib => "will_paginate", :version => ">=2.3.11"
   config.gem "activemerchant", :lib => "active_merchant", :version => ">=1.4.2"
 
