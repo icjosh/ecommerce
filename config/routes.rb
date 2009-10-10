4b77af091c973bfa96486ddf4c319d0bab905b39
Adrian Perez <adrianperez.deb@gmail.com>
2009-10-10 16:32:24 -0400

    Add User resource for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :users
+
   map.resources :products
   map.resources :orders
   map.store '/store', :controller => 'store', :action => 'index'
