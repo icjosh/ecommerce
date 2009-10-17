33ed83d03c0231c54d2dc15e312ab5c61f509f5c
Adrian Perez <adrianperez.deb@gmail.com>
2009-10-17 14:56:23 -0400

    Add about menu javascript file and minor changes in app layout

--- public/javascripts/about.js
+++ public/javascripts/about.js
@@ -1,1 +1,10 @@
-/home/adrian/Development/projects/ecommerce/app/views/front/about_dialog.js
+var dialog = new dojox.widget.Dialog({
+    dimensions:[640,420]
+},"about-dialog");
+
+dialog.startup();
+
+// setup a way to show it
+dojo.connect(dojo.byId("about"), "onclick", dialog, "show");
+
+	
