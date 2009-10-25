eb5a679df5ee83d59dbc00e27dde34422e963637
Adrian Perez <adrianperez.deb@gmail.com>
2009-10-25 15:22:36 -0500

    Add "add" and "redirect_to_index" actions for CartController

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,14 +1,24 @@
 class CartController < ApplicationController
-  before_filter :initialize_cart
-
-  private
-  def initialize_cart
-    if session[:cart_id]
-      @cart = Cart.find(session[:cart_id])
+  
+  def add
+    begin
+      @product = Product.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      logger.error("Attempt to access invalid product #{params[:id]}")
+      redirect_to_index "Invalid product"
     else
-      @cart = Cart.create
-      session[:cart_id] = @cart.id
+      @item = @cart.add(params[:id])
+      respond_to do |format|
+        format.html { redirect_to_index }
+        format.js
+      end
     end
+  end
+  
+  def redirect_to_index(msg = nil)
+    flash[:notice] = msg if msg
+    # TODO: fix this with ajax
+    redirect_to products_path
   end
 
 end
