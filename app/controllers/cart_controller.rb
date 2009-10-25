0a26f7c11500e90e5306ae9f87060b0408cc3d6c
Adrian Perez <adrianperez.deb@gmail.com>
2009-10-24 20:44:30 -0400

    Make initialize_controller private in CartController. Remove store views

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,6 +1,7 @@
 class CartController < ApplicationController
   before_filter :initialize_cart
-  
+
+  private
   def initialize_cart
     if session[:cart_id]
       @cart = Cart.find(session[:cart_id])
