919beca9c6fccca3ee07ec851fa3a9fbcbd898b0
Adrian Perez <adrianperez.deb@gmail.com>
2009-10-25 16:05:22 -0500

    Add "Add to Cart" button as a helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,14 @@ module ApplicationHelper
     content_for(:head, content)
   end
 
+  def add_product_to_cart_button(product)
+    button_to "Add to Cart", {
+      :controller => 'Cart',
+      :action 	  => 'add',
+      :id 	  => product
+    },
+    :dojoType 	=> "dijit.form.Button",
+    :iconClass => "addToCartIcon",
+    :label => "Add to Cart"
+  end
 end
